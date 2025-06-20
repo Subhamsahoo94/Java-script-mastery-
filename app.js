d7963da97a9f17545b186a67b2c08fe6314c646f
Subham sahoo <subhamsahoo0p90@gmail.com>
2025-06-20 20:20:49 +0530

    Day-3
    It's  day 3,

---
+++ app.js
@@ -0,0 +1,73 @@
+// ---------------------------------------------IF STMT----------------------------------------------
+// let light = "GREEN";
+// let RED,YELLOW,GREEN;
+
+// if(light == "RED"){
+
+// console.log('STOP');
+
+// }
+
+// if(light== "YELLOW"){
+
+// console.log('GO SLOW');
+
+// }
+
+// if(light=="GREEN"){
+
+// console.log('YOU CAN GO NOW');
+
+// }
+// console.log("thank uou")
+
+// ---------------------------------------------IF ELSE AND ELSE IF STMT----------------------------------------------
+
+
+// let size = "S";
+
+// if(size=="XL"){
+//     console.log("price: 250rs");
+// }
+// else if(size=="L"){
+//     console.log("price: 200rs");
+
+// }
+// else if(size=="M"){
+//     console.log("price: 100rs");
+
+// }
+// else{
+//     console.log("price: 50rs");
+
+// }
+
+// ---------------------------------------------NESTED IF ELSE STMT----------------------------------------------
+
+// let mark = "40";
+
+// if(mark<=33){
+//     console.log('better luck next time!');
+//     if(mark<=80){
+//         console.log('your grade is A');
+
+//     }
+//     else{
+//         console.log('your grade is A')
+//     }
+// }
+// else{
+//     console.log('you are pass');
+// }
+// ---------------------------------------------logical oprators----------------------------------------------
+
+
+// let str = "apple";
+
+// if((str[0]==="a"&&(str>3))){
+//     console.log(`${str} is a good string`);
+// }
+// else{
+//     console.log(`${str} is a not good string`);
+
+// }
