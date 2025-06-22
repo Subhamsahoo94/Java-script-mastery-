96f11f756e7d47c6430aef57eea05c8a2322cbae
Subham sahoo <subhamsahoo0p90@gmail.com>
2025-06-22 16:28:29 +0530

    Day-5
    Day-5

---
+++ app (4).js
@@ -0,0 +1,30 @@
+// ---------- array constant 
+ 
+// let arr = [1,2,3];
+
+// console.log(arr);
+
+// arr.push(4);
+// console.log(arr);
+// array are constan but we cannot change there addres but change the value in them.
+
+// ---------- nested array
+
+// let names = [["ram"],["sam"],["sam"]];
+
+// console.log(names);
+// console.log(names[0]);
+// console.log(names[0][0][0]);
+
+//---------- practice Qs
+
+// let game = [['x',null,'o'], [null,'x',null],['o',null,'x']];
+
+// console.log(game);
+// //  change
+// game[1][0] = 'x';
+// console.log(game);
+
+// game[1][2] = 'x';
+// console.log(game);
+
